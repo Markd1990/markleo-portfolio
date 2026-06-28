a9f7d4cfbad7674935272fb9be71ce0caa7450a6
mark <markleobostrillo@gmail.com>
2026-06-28 23:02:22 +0800

    Partial  update  mobile responsive

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const modalContainer = document.getElementById("modalContainer");
 const readMorebtn = document.getElementById("readMorebtn");
 const modalCloseBtn = document.getElementById("modalCloseBtn");
+const mobNavLinkContainer = document.getElementById("mobNavLinkContainer");
+const menuIcon = document.getElementById("menuIcon");
+const closeNavBtn = document.getElementById("closeNavBtn");
 
 readMorebtn.addEventListener("click", () => {
   modalContainer.classList.remove("hidden");
@@ -9,5 +12,16 @@ readMorebtn.addEventListener("click", () => {
 
 modalCloseBtn.addEventListener("click", () => {
   modalContainer.classList.add("hidden");
+  document.body.classList.remove("overflow-hidden");
+});
+
+menuIcon.addEventListener("click", () => {
+  mobNavLinkContainer.classList.remove("hidden");
+  mobNavLinkContainer.classList.add("flex");
+  document.body.classList.add("overflow-hidden");
+});
+
+closeNavBtn.addEventListener("click", () => {
+  mobNavLinkContainer.classList.add("hidden");
   document.body.classList.remove("overflow-hidden");
 });
